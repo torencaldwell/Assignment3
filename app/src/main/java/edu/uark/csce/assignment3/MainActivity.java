fc06e73fa6de07ac08a79ab2949ea2f048ef3289
Toren <torencaldwell@gmail.com>
2017-10-27 11:39:36 -0500

    Added permissions

--- app/src/main/java/edu/uark/csce/assignment3/MainActivity.java
+++ app/src/main/java/edu/uark/csce/assignment3/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.uark.csce.assignment3;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.hardware.camera2.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textview = (TextView)findViewById(R.id.cam_textview);
+        if(checkCameraHardware(getApplicationContext())){
+            textview.setText("I have a camera");
+        }else{
+            textview.setText("I don't have a camera");
+        }
     }
+
+    private boolean checkCameraHardware(Context context){
+        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
+            return true;
+        return false;
+    }
+
+
 }
